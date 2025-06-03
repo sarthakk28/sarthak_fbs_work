6bb43f2bf60faed049d8bfc6d493999cee5e8e4e
sarthakk28 <165922829+sarthakk28@users.noreply.github.com>
2025-06-03 20:13:21 +0530

    Add files via upload

---
+++ even or odd.c
@@ -0,0 +1,12 @@
+void main()
+{
+	int no = 27;
+	
+	if(no%2==0){
+		printf("even");
+	}
+		else{
+			printf("odd");
+		}
+	
+}
---
+++ swaping to value using temp.c
@@ -0,0 +1,10 @@
+//Write a C program to swap two numbers using a temporary third variable.
+void main()
+{
+   int a=10,b=20,temp;
+   temp = a;
+   a=b;
+   b=temp;
+   printf("a= %d :b= %d  ",a,b);
+   	
+}
