e90e6596af3e8067d06ebd8d7ef8ea3a3986ae84
sarthakk28 <165922829+sarthakk28@users.noreply.github.com>
2025-06-05 09:57:52 +0530

    Add files via upload

---
+++ C Programming/Assignments/Assignment 1/find perimeter.c
@@ -0,0 +1,10 @@
+//Write a C program to input the length and width of a rectangle and find its perimeter.
+void main()
+{
+  int length = 15,width = 24,perimeter;
+  
+  perimeter = 2*(length+width);
+
+  printf("perimeter is : %d",perimeter);
+  return 0;  
+}
