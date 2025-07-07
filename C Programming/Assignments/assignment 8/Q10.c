c9c9da8f6aae3bff2f4a2caec01ccd92cf9708d7
sarthakk28 <165922829+sarthakk28@users.noreply.github.com>
2025-07-07 10:27:32 -0700

    Delete C Programming/Assignments/assignment 8/Q10.c

--- C Programming/Assignments/assignment 8/Q10.c
+++
@@ -1,9 +0,0 @@
-
-//strdup () Set all characters in string to given character
-#include <stdio.h>
-#include <string.h>
-void main () {
-	char str1 []="chaitanya";
-	strset (str1,'#');
-	printf("%s",str1);
-}
